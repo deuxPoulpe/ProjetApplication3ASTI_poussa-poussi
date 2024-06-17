82d5aeaa5803ad6f95b68a22bc9159bfb5c965d7
unPoulpe <yayafeu2@gmail.com>
2024-06-17 16:52:17 +0200

    up

--- src/myPackage/Game.java
+++ src/myPackage/Game.java
@@ -46,7 +46,19 @@ public class Game {
             grid.display();
         }
     }
-        
+    
+    /**
+     * Cette méthode gère un tour de jeu.
+     * Elle affiche d'abord le tour du joueur actuel.
+     * Ensuite, elle demande au joueur de placer un jeton sur le plateau.
+     * Si le placement du jeton est invalide (placeToken() renvoie 1), elle continue à demander jusqu'à ce qu'un placement valide soit fait.
+     * Ensuite, elle affiche l'état actuel du plateau de jeu.
+     * Après cela, elle demande au joueur de pousser un jeton dans une direction spécifiée.
+     * Si le mouvement est invalide (pushToken() renvoie 1), elle continue à demander jusqu'à ce qu'un mouvement valide soit fait.
+     * Ensuite, elle change le tour du joueur.
+     * Enfin, elle vérifie si le jeu est terminé ou si des points ont été marqués.
+     * Si le jeu est terminé, elle affiche le gagnant et termine le programme.
+     */
     public void gameTurn() {
         System.out.println("Player " + this.playerTurn + " turn");
 
@@ -78,11 +90,20 @@ public class Game {
         } else if (gameState == 1) {
             System.out.println("Y wins");
             System.exit(0);
-        }
-        
-        
+        } 
     }
 
+    /**
+     * Cette méthode gère le placement d'un jeton sur le plateau de jeu.
+     * Elle lit d'abord les coordonnées x et y de l'entrée de l'utilisateur.
+     * Ensuite, elle crée un objet Coordinates avec ces coordonnées.
+     * Après cela, elle tente de placer un jeton du joueur actuel à ces coordonnées sur le plateau de jeu.
+     * Si le placement du jeton est invalide (par exemple, si les coordonnées sont hors du plateau ou si un jeton est déjà présent à ces coordonnées), 
+     * la méthode placeToken de l'objet grid lancera une IllegalArgumentException.
+     * Cette exception est attrapée et son message est affiché à l'utilisateur.
+     * Dans ce cas, la méthode placeToken renvoie 1 pour indiquer une erreur.
+     * Si le jeton est placé avec succès, la méthode renvoie 0.
+     */
     public int placeToken() {
         int result = 0;
 
@@ -142,6 +163,11 @@ public class Game {
 
     }
 
+    /**
+     * Cette méthode permet à un joueur de retirer deux jetons d'une ligne de cinq jetons alignés.
+     * Elle prend en paramètre une liste de coordonnées représentant l'alignement de cinq jetons.
+     * Elle demande au joueur de saisir les coordonnées de deux jetons qu'il souhaite retirer.
+     */
     public void removeTwoTokens(List<Coordinates> alignment) throws Exception {
         String index = "first";
         for (int i = 0; i < 2; i++) {
